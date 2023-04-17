10171b712afbae0ca059e3217743684f9069806a
norhther <omar.lopez@est.fib.upc.edu>
2023-04-17 14:19:36 +0200

    Fix modeling code

--- session4/run.sh
+++ session4/run.sh
@@ -2,32 +2,32 @@
 
 BASEDIR=../DDI
 
-./corenlp-server.sh -quiet true -port 9000 -timeout 15000  &
-sleep 1
+# ./corenlp-server.sh -quiet true -port 9000 -timeout 15000  &
+# sleep 1
 
 # extract features
-echo "Extracting features"
-python3 extract-features.py $BASEDIR/data/devel/ > devel.cod &
-python3 extract-features.py $BASEDIR/data/train/ | tee train.cod | cut -f4- > train.cod.cl
+# echo "Extracting features"
+# python3 extract-features.py $BASEDIR/data/devel/ > devel.cod &
+# python3 extract-features.py $BASEDIR/data/train/ | tee train.cod | cut -f4- > train.cod.cl
 
-kill `cat /tmp/corenlp-server.running`
+# kill `cat /tmp/corenlp-server.running`
 
 # train model
-echo "Training model"
-python3 train-sklearn.py model.joblib vectorizer.joblib < train.cod.cl
-# run model
-echo "Running model..."
-python3 predict-sklearn.py model.joblib vectorizer.joblib < devel.cod > devel.out
-# evaluate results
-echo "Evaluating results..."
-python3 evaluator.py DDI $BASEDIR/data/devel/ devel.out > devel.stats
-
-
-# echo "Training model SGD"
-# python3 train-sgd.py sgd.joblib vectorizer.joblib < train.cod.cl
+# echo "Training model"
+# python3 train-sklearn.py model.joblib vectorizer.joblib < train.cod.cl
 # run model
 # echo "Running model..."
-# python3 predict-sklearn.py sgd.joblib vectorizer.joblib < devel.cod > devel.out
+# python3 predict-sklearn.py model.joblib vectorizer.joblib < devel.cod > devel.out
 # evaluate results
 # echo "Evaluating results..."
-# python3 evaluator.py DDI $BASEDIR/data/devel/ devel.out > devel-sgd.stats
+# python3 evaluator.py DDI $BASEDIR/data/devel/ devel.out > devel.stats
+
+
+echo "Training model SGD"
+python3 train-sgd.py sgd.joblib vectorizer.joblib < train.cod.cl
+# run model
+echo "Running model..."
+python3 predict-sklearn.py sgd.joblib vectorizer.joblib < devel.cod > devel.out
+# evaluate results
+echo "Evaluating results..."
+python3 evaluator.py DDI $BASEDIR/data/devel/ devel.out > devel-sgd.stats
